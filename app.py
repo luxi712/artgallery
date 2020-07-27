ba971df9630814d9ae986c0d63a78683385a9548
Your Name <you@example.com>
2020-07-27 14:20:00 +0000

    cards

--- app.py
+++ app.py
@@ -1,16 +1,43 @@
-# ---- YOUR APP STARTS HERE ----
+ # ---- YOUR APP STARTS HERE ----
 # -- Import section --
+from datetime import datetime
 from flask import Flask
 # from flask import render_template
 # from flask import request
+from flask import render_template
+from flask import request
+
 
 
 # -- Initialization section --
 app = Flask(__name__)
-
-
 # -- Routes section --
 @app.route('/')
 @app.route('/index')
 def index():
     return "hello world"
+    return render_template('nav.html', time=datetime.now())
+
+@app.route('/basquiat')
+def basquiat():
+    return render_template('basquiat.html', time=datetime.now)
+
+@app.route('/monet')
+def monet():
+    return render_template('Monet.html', time=datetime.now)
+
+@app.route('/close')
+def close():
+    return render_template('Close.html', time=datetime.now)
+
+@app.route('/kahlo')
+def kahlo():
+    return render_template('Kahlo.html', time=datetime.now)
+
+@app.route('/paintings')
+def paintings():
+    return render_template('paintings.html', time=datetime.now)
+
+@app.route('/sculptures')
+def sculptures():
+    return render_template('sculptures.html', time=datetime.now) 
